7aee2bf2e78ae94fcf94ccfb5b8b90c0b6244d76
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-02-28 19:43:15 -0500

    styling of main.html

--- js/app.js
+++ js/app.js
@@ -10,5 +10,19 @@
         templateUrl: 'views/main.html',
         controller:'MainController as mainCtrl'
       })
+      .when('/mansions', {
+        templateUrl: 'views/products.html',
+        controller:'MainController as mainCtrl'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller:'MainController as mainCtrl'
+      })
+      .when('/not-found', {
+        templateUrl: 'views/not-found.html',
+      })
+      .otherwise({
+        redirectTo: '/not-found'
+      });
   });
 })();
--- js/controller.js
+++ js/controller.js
@@ -2,9 +2,19 @@
   "use strict";
 
   angular.module('store')
-    .controller('MainController', function(MansionSaleService, $scope) {
+    .controller('MainController', function(MansionSaleService, $scope, $location) {
       var mainCtrl = this;
 
       mainCtrl.mansion = MansionSaleService.getMansions();
+
+      //routes
+      mainCtrl.productPage = function () {
+      $location.path('/mansions');
+    };
+
+      mainCtrl.adminPage = function () {
+        $location.path('/admin');
+      };
+
     });
   })();
