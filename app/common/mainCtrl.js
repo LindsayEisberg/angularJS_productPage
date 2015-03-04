88500bf4b0deb66ecd384fdf19506ef6de7900af
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-03-04 16:40:30 -0500

    updated director to include yo generator

---
+++ app/common/mainCtrl.js
@@ -0,0 +1,17 @@
+(function () {
+  "use strict";
+  angular.module("bootStruck")
+    .controller("MainCtrl", function () {
+      var mainCtrl = this;
+
+      /////splash page Routing/////
+      mainCtrl.productPage = function () {
+      $location.path('../store/boots');
+    };
+
+      mainCtrl.adminPage = function () {
+        $location.path('../store/admin');
+      };
+
+  });
+})();
---
+++ app/store/store.module.js
@@ -0,0 +1,32 @@
+(function (){
+  "use strict";
+
+  angular.module('store',[
+    'ngRoute',
+  ])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/store/boots', {
+        templateUrl: 'store/views/products.html',
+        controller:'ProductController as productCtrl'
+      })
+      .when('/store/admin', {
+        templateUrl: 'store/views/admin.html',
+        controller:'AdminController as adminCtrl'
+      })
+      .when('/store/newListing', {
+        templateUrl: 'store/views/newListing.html',
+        controller: "AdminController as adminCtrl"
+      })
+      .when('/store/boots/:detailPage', {
+        templateUrl: 'store/views/detailPage.html',
+        controller:'ProductController as productCtrl'
+      })
+      .when('/store/cart', {
+        templateUrl: 'store/views/addToCart.html',
+        controller: "CartController as cartCtrl"
+      });
+  })
+  .constant('_', _);
+
+})();
