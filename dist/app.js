7fb193378726257af6d312c7ed9b83497629f43e
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-03-04 16:41:34 -0500

    added dist

--- dist/app.js
+++ dist/app.js
@@ -25,3 +25,60 @@
     // 'ngAria',
     'store'
 ])));
+
+(function () {
+  "use strict";
+  angular.module("bootStruck")
+    .controller("MainCtrl", function () {
+      var mainCtrl = this;
+
+      /////splash page Routing/////
+      mainCtrl.productPage = function () {
+      $location.path('../store/boots');
+    };
+
+      mainCtrl.adminPage = function () {
+        $location.path('../store/admin');
+      };
+
+  });
+})();
+
+(function (){
+  "use strict";
+
+  angular.module('store',[
+    'ngRoute',
+  ])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/store/boots', {
+        templateUrl: 'store/views/products.html',
+        controller:'ProductController as productCtrl'
+      })
+      .when('/store/admin', {
+        templateUrl: 'store/views/admin.html',
+        controller:'AdminController as adminCtrl'
+      })
+      .when('/store/newListing', {
+        templateUrl: 'store/views/newListing.html',
+        controller: "AdminController as adminCtrl"
+      })
+      .when('/store/boots/:detailPage', {
+        templateUrl: 'store/views/detailPage.html',
+        controller:'ProductController as productCtrl'
+      })
+      .when('/store/cart', {
+        templateUrl: 'store/views/addToCart.html',
+        controller: "CartController as cartCtrl"
+      });
+  })
+  .constant('_', _);
+
+})();
+
+
+
+
+
+
